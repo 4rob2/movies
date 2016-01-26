616b77cae6c9c8133a6b38aa01a3c6dd1f167e7c
Chris Kuiper and rob reasoner <student@epicodus.com>
2016-01-26 15:22:19 -0800

    add senior discount specs with tests successful

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,10 @@
-function Ticket(generalPrice) {
+function Ticket(generalPrice, seniorPrice) {
   this.generalPrice = 10;
+  var generalPrice = 10;
+
+  this.seniorPrice = (generalPrice * .75);
+
 }
+//  function Ticket(seniorPrice) {
+//   var seniorPrice = (generalPrice * .75);
+// }
--- spec/specs.js
+++ spec/specs.js
@@ -3,4 +3,9 @@ describe('Ticket', function() {
     var testTicket  = new Ticket(10);
     expect(testTicket.generalPrice).to.equal(10);
   });
+
+  it("will give a discounted price for a Senior Admission", function() {
+    var testTicket  = new Ticket(7.50);
+    expect(testTicket.seniorPrice).to.equal(7.50);
+  });
 });
