1cbafb034123a0a503138f87d2aaf9cf79327709
Chris Kuiper and rob reasoner <reasoner55@hotmail.com>
2016-01-26 16:26:50 -0800

    add content to html

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,7 @@
-function Ticket(generalPrice, seniorPrice, youthPrice) {
+function Ticket(generalPrice, seniorPrice, youthPrice, matineePrice) {
   this.generalPrice = 10;
   var generalPrice = 10;
   this.seniorPrice = (generalPrice * .75);
   this.youthPrice = (generalPrice * .5);
+  this.matineePrice = (generalPrice * .625);
 }
--- spec/specs.js
+++ spec/specs.js
@@ -12,6 +12,10 @@ describe('Ticket', function() {
   it("will give a discounted price for a Youth Admission", function() {
     var testTicket  = new Ticket(5.00);
     expect(testTicket.youthPrice).to.equal(5.00);
+  });
 
+  it("will give a discounted price for a Matinee Admission", function() {
+    var testTicket  = new Ticket(6.25);
+    expect(testTicket.matineePrice).to.equal(6.25);
   });
 });
